2c6b883e4a40a9ffb15f7f1c6808b4689c7368f7
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-10-03 17:15:40 +0700

    working with mongoose

---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+const express = require('express')
+
+const shopController = require('../controllers/shop')
+
+const router = express.Router()
+
+module.exports = router
