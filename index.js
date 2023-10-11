fa509556926c7e96e8dcaddce852dea10067ca1a
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-10-11 10:53:23 +0700

    oke

--- index.js
+++ index.js
@@ -1,68 +1,52 @@
 const express = require('express')
 const session = require('express-session')
+const csurf = require('csurf')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
-const port = 3000
 
-app.use(
-    session({
-        secret: 'mysecretkey',
-        resave: false,
-        saveUninitialized: true,
-    })
-)
-app.use(bodyParser.urlencoded({ extended: true }))
+//Sử dụng express-session để quản lý session
+// app.use(
+//     session({
+//         secret: 'dnh', //
+//         resave: false,
+//         saveUninitialized: true,
+//         cookie: { secure: false }, //tắt yêu cầu HTTPS
+//     })
+// )
 
-app.get('/', (req, res) => {
-    if (req.session.username) {
-        // Hiển thị danh sách bài viết của người dùng đã đăng nhập
-        const userPosts = req.session.userPosts || []
-        res.send(`Welcome, ${req.session.username}!<br> Your Posts: ${userPosts.join(', ')}`)
-    } else {
-        res.send('Ban chua dang nhap')
-    }
+// Sử dụng bodyParser để xử lý dữ liệu POST
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Sử dụng csurf middleware với cấu hình sử dụng session
+app.use(cookieParser())
+
+const csrfProtection = csurf({
+    cookie: true, //
 })
 
-app.get('/login', (req, res) => {
+// Gắn middleware csrfProtection cho tất cả các route cần bảo vệ
+app.use(csrfProtection)
+
+app.get('/', (req, res) => {
+    // Lấy CSRF token từ session
+    const csrfToken = req.csrfToken()
     res.send(`
-    <form method="POST" action="/login">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username"><br>
-      <input type="submit" value="Login">
+    <h1>Truy cập được</h1>
+    <form method="post" action="/process">
+      <input type="hidden" name="_csrf" value="${csrfToken}">
+      <input type="text" name="data" placeholder="Data">
+      <button type="submit">Submit</button>
     </form>
   `)
 })
 
-app.post('/login', (req, res) => {
-    // Xử lý đăng nhập và lưu trạng thái đăng nhập vào session
-    const username = req.body.username
-    if (username) {
-        req.session.username = username
-        req.session.userPosts = []
-        res.redirect('/')
-    } else {
-        res.send('Please enter a valid username.')
-    }
+app.post('/process', (req, res) => {
+    const data = req.body.data
+    res.send(`Dữ liệu đã được xử lý: ${data}`)
 })
 
-app.post('/post', (req, res) => {
-    if (req.session.username) {
-        // Thêm bài viết mới vào danh sách của người dùng
-        const newPost = req.body.post
-        req.session.userPosts.push(newPost)
-        res.redirect('/')
-    } else {
-        res.redirect('/login')
-    }
-})
-
-app.get('/logout', (req, res) => {
-    // Xóa trạng thái đăng nhập từ session
-    req.session.destroy()
-    res.redirect('/login')
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(3000, () => {
+    console.log('Server đang chạy trên cổng 3000')
 })
