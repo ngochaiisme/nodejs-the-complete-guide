00c0ed6932c5e6bb895a8e961027ee4ecfbe21ac
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-10-18 11:33:43 +0700

    update post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator')
 const Post = require('../models/post')
+const fs = require('fs')
+const path = require('path')
 exports.getPosts = (req, res, next) => {
     Post.find()
         .then((posts) => {
@@ -71,4 +73,56 @@ exports.getPost = (req, res, next) => {
             }
             next(err)
         })
+}
+
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.')
+        error.statusCode = 422
+        throw error
+    }
+
+    const postId = req.params.postId
+    const title = req.body.title
+    const content = req.body.content
+    let imageUrl = req.body.image
+    if (req.file) {
+        imageUrl = '/' + req.file.path.replace(/\\/g, '/')
+    }
+    if (!imageUrl) {
+        const error = new Error('No image picked.')
+        error.statusCode = 422
+        throw error
+    }
+
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Can not find post.')
+                error.statusCode = 404
+                throw error
+            }
+            if (imageUrl != post.imageUrl) {
+                clearImage(post.imageUrl)
+            }
+            post.title = title
+            post.content = content
+            post.imageUrl = imageUrl
+            return post.save()
+        })
+        .then((result) => {
+            res.status(200).json({ message: 'Post updated', post: result })
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
+
+const clearImage = (Path) => {
+    Path = path.join(__dirname, '..', Path)
+    fs.unlink(Path, (err) => console.log(err))
 }
